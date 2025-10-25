de96087e36b06c6a1e4e8dd0dae1e3799b7dfbe0
sohamkk2004 <sohamkk2004@gmail.com>
2025-10-24 22:38:16 -0500

    Add files via upload

---
+++ mode.rkt
@@ -0,0 +1,7 @@
+(define prompt?
+   (let [(args (current-command-line-arguments))]
+     (cond
+       [(= (vector-length args) 0) #t]
+       [(string=? (vector-ref args 0) "-b") #f]
+       [(string=? (vector-ref args 0) "--batch") #f]
+       [else #t])))
